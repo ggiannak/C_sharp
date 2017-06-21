315fb5e234e5c342039e0d3120b1ec2959741372
george <ggiannak@yahoo.gr>
2017-06-21 15:27:40 +0300

    Add existing file

---
+++ FunWithArrays/FunWithArrays/Array2D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FunWithArrays
+{
+    class Array2D
+    {
+        static void Main(string[] args)
+        {
+            int[,] twoDArray = { { 3, 2 }, { 4, 5 }, { 5, 6 } };
+            for (int x=0; x < twoDArray.GetLength(0); x++)
+            {
+                for (int y = 0; y < twoDArray.GetLength(1); y++)
+                {
+                    int value = twoDArray[x, y];
+                    Console.WriteLine(value.ToString());
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
